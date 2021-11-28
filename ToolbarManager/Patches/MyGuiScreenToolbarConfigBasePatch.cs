5682ebc2fe7f933c6d8879ccd7b21148e118c318
Viktor Ferenczi <viktor@ferenczi.eu>
2021-11-28 03:31:13 +0100

    Support for block toolbars (#2)
    * v1.1
    
    * Support for block toolbars

--- ToolbarManager/Patches/MyGuiScreenToolbarConfigBasePatch.cs
+++ ToolbarManager/Patches/MyGuiScreenToolbarConfigBasePatch.cs
@@ -1,9 +1,13 @@
 using System;
 using HarmonyLib;
 using Sandbox.Game.Gui;
+using Sandbox.Game.Screens.Helpers;
+using Sandbox.Game.World;
 using Sandbox.Graphics.GUI;
 using VRage.Game;
 using VRageMath;
+
+// ReSharper disable UnusedMember.Local
 
 // ReSharper disable UnusedType.Global
 
@@ -14,18 +18,18 @@ namespace ToolbarManager.Patches
     [HarmonyPatch(typeof(MyGuiScreenToolbarConfigBase))]
     public static class MyGuiScreenToolbarConfigBasePatch
     {
-        public static event Action OnSaveCharacterToolbar;
-        public static event Action OnLoadCharacterToolbar;
+        public static event Action OnSaveToolbar;
+        public static event Action OnLoadToolbar;
 
         [HarmonyPostfix]
         [HarmonyPatch(nameof(MyGuiScreenToolbarConfigBase.RecreateControls))]
-        private static void RecreateControlsPostfix(MyGuiScreenToolbarConfigBase __instance, bool contructor) // sic!
+        private static void RecreateControlsPostfix(MyGuiScreenToolbarConfigBase __instance)
         {
             createSaveButton(__instance);
             createLoadButton(__instance);
         }
 
-        private static void createSaveButton(MyGuiScreenToolbarConfigBase __instance)
+        private static void createSaveButton(MyGuiScreenToolbarConfigBase screen)
         {
             var button = new MyGuiControlButton
             {
@@ -34,11 +38,11 @@ namespace ToolbarManager.Patches
                 VisualStyle = MyGuiControlButtonStyleEnum.Small,
                 Position = new Vector2(-0.425f, 0.41f)
             };
-            button.ButtonClicked += _ => OnSaveCharacterToolbar?.Invoke();
-            __instance.Elements.Add(button);
+            button.ButtonClicked += _ => OnSaveToolbar?.Invoke();
+            screen.Elements.Add(button);
         }
 
-        private static void createLoadButton(MyGuiScreenToolbarConfigBase __instance)
+        private static void createLoadButton(MyGuiScreenToolbarConfigBase screen)
         {
             var button = new MyGuiControlButton
             {
@@ -47,8 +51,8 @@ namespace ToolbarManager.Patches
                 VisualStyle = MyGuiControlButtonStyleEnum.Small,
                 Position = new Vector2(-0.425f, 0.46f)
             };
-            button.ButtonClicked += _ => OnLoadCharacterToolbar?.Invoke();
-            __instance.Elements.Add(button);
+            button.ButtonClicked += _ => OnLoadToolbar?.Invoke();
+            screen.Elements.Add(button);
         }
     }
 }
