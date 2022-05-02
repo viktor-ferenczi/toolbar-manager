bfc3c7be89d326b5cbfa49ce00470de73dc9d0ec
Viktor Ferenczi <viktor@ferenczi.eu>
2022-05-02 02:25:08 +0200

    Backslash key to open the menu with an empty search bar

--- ToolbarManager/Patches/MyGuiScreenGamePlayPatch.cs
+++ ToolbarManager/Patches/MyGuiScreenGamePlayPatch.cs
@@ -2,9 +2,8 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Text;
 using HarmonyLib;
-using Sandbox.Game.Entities;
+using Sandbox.Game.Entities.Character;
 using Sandbox.Game.Gui;
-using Sandbox.Game.Screens.Helpers;
 using Sandbox.Game.World;
 using Sandbox.Graphics.GUI;
 using ToolbarManager.Gui;
@@ -14,9 +13,11 @@ namespace ToolbarManager.Patches
 {
     [HarmonyPatch(typeof(MyGuiScreenGamePlay))]
     [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    // ReSharper disable once UnusedType.Global
     public static class MyGuiScreenGamePlayPatch
     {
         private static readonly List<MyKeys> PressedKeys = new List<MyKeys>();
+
         private static readonly StringBuilder MatchingKeys = new StringBuilder();
         // private static readonly ListDialog ListDialog = new ListDialog();
 
@@ -24,45 +25,52 @@ namespace ToolbarManager.Patches
         [HarmonyPatch(nameof(MyGuiScreenGamePlay.HandleUnhandledInput))]
         public static bool HandleUnhandledInputPrefix()
         {
-            // TODO: Add configuration dialog with checkbox to disable this feature
-            if (MyGuiScreenGamePlay.ActiveGameplayScreen == null)
+            if (MyGuiScreenGamePlay.ActiveGameplayScreen != null || !(MySession.Static.ControlledEntity is MyCharacter))
+                return true;
+
+            var myInput = MyInput.Static;
+            if (myInput.IsAnyShiftKeyPressed() && myInput.IsAnyCtrlKeyPressed())
             {
                 GetPressedKeys();
                 if (MatchingKeys.Length != 0)
                 {
-                    OpenCubeBuilder();
+                    OpenCubeBuilder(MatchingKeys.ToString());
                     return false;
                 }
             }
+            
+            if (myInput.IsNewKeyPressed(MyKeys.OemPipe))
+            {
+                OpenCubeBuilder("");
+                return false;
+            }
+
             return true;
         }
 
-        private static void OpenCubeBuilder()
+        private static void OpenCubeBuilder(string searchText)
         {
             if (!(MyGuiSandbox.CreateScreen(typeof(CustomToolbarConfigScreen), 0, null, null) is CustomToolbarConfigScreen dialog))
                 return;
-            
+
             MyGuiScreenGamePlay.ActiveGameplayScreen = dialog;
             MyGuiSandbox.AddScreen(dialog);
-            
-            dialog.SetSearchText(MatchingKeys.ToString());
+
+            dialog.SetSearchText(searchText);
         }
 
         private static void GetPressedKeys()
         {
             MatchingKeys.Clear();
-            
+
             var myInput = MyInput.Static;
-            if (!(myInput.IsAnyCtrlKeyPressed() && myInput.IsAnyShiftKeyPressed()))
-                return;
-            
             myInput.GetPressedKeys(PressedKeys);
             foreach (var key in PressedKeys)
             {
                 var name = myInput.GetKeyName(key);
                 if (name.Length != 1)
                     continue;
-                    
+
                 var c = name[0];
                 if (c >= 'A' && c <= 'Z')
                     MatchingKeys.Append(c);
